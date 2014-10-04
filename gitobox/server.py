4f837e794b8ec9e01ffa71eb711fc6a2052f87b0
Remi Rampin <remirampin@gmail.com>
2014-10-04 14:25:05 -0400

    Adds TCP server to receive hook notifications

---
+++ gitobox/server.py
@@ -0,0 +1,91 @@
+from __future__ import unicode_literals
+
+import logging
+import select
+import socket
+import time
+
+from gitobox.utils import iteritems, itervalues
+
+
+class Server(object):
+    TIMEOUT = 5.0
+    LENGTH = 1024
+
+    def __init__(self, port, callback):
+        self._callback = callback
+        self._port = port
+
+    def run(self):
+        clients = {}
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', self._port))
+        server.listen(5)
+
+        next_timeout = None
+        now = time.time()
+
+        while True:
+            sockets = [server]
+            sockets.extend(clients)
+            timeout = (None if next_timeout is None
+                       else next_timeout - now + 0.2)
+            print("Timeout: %r" % timeout)
+            rlist, _, _ = select.select(sockets, [], [],
+                                        timeout)
+            now = time.time()
+
+            # Timeouts
+            for sock, (data, timeout, addr) in list(iteritems(clients)):
+                if now > timeout:
+                    del clients[conn]
+                    conn.send(b"timed out\nERROR\n")
+                    conn.close()
+                    logging.debug("Connection from %s timed out",
+                                  addr)
+                    next_timeout = -1
+
+            for sock in rlist:
+                if sock == server:
+                    conn, addr = server.accept()
+                    logging.debug("Connection from %s", addr)
+                    timeout = now + self.TIMEOUT
+                    clients[conn] = b'', timeout, addr
+                    if next_timeout is None:
+                        next_timeout = timeout
+                    else:
+                        next_timeout = min(next_timeout, timeout)
+                else:
+                    data, timeout, addr = clients[conn]
+                    res = conn.recv(self.LENGTH - len(data))
+                    if res:
+                        end = res.find(b'\n')
+                        if end != -1:
+                            end += len(data)
+                            data += res[:end]
+                            res = None
+                        else:
+                            data += res
+                    if not res or len(data) >= self.LENGTH:
+                        del clients[conn]
+                        try:
+                            self._callback(data, conn, addr)
+                        except Exception:
+                            conn.send(b"internal server error\nERROR\n")
+                            raise
+                        finally:
+                            conn.close()
+                        next_timeout = -1
+                    else:
+                        clients[conn] = data, timeout, addr
+
+            if next_timeout == -1:
+                if clients:
+                    next_timeout = min(t
+                                       for _, t, _ in itervalues(clients))
+                    print("min(%r) = %r" % (
+                          [t for _, t, _ in itervalues(clients)],
+                          next_timeout))
+                else:
+                    next_timeout = None
--- gitobox/sync.py
+++ gitobox/sync.py
@@ -1,24 +1,60 @@
 from __future__ import unicode_literals
 
 import logging
+from threading import RLock, Thread
 
+from gitobox.server import Server
 from gitobox.utils import unicode_
 from gitobox.watch import DirectoryWatcher
 
 
 class Synchronizer(object):
+    """Main application logic: synchronizes folder with Git repo.
+    """
     def __init__(self, folder, repository, branchname, timeout):
         self._watcher = DirectoryWatcher(folder,
                                          self._directory_changed,
                                          timeout)
+        self._hook_server = Server(5055, self._hook_triggered)
+        self._lock = RLock()
         #self._repository = GitRepository(repository, branchname)
 
     def run(self):
-        self._watcher.run()
+        # TODO : Check that current Git tree matches directory contents
+
+        watcher_thread = Thread(target=self._watcher.run)
+        watcher_thread.setDaemon(True)
+        watcher_thread.start()
+
+        self._hook_server.run()
 
     def _directory_changed(self, paths):
-        logging.warning("Paths changed: %s",
-                        " ".join(unicode_(p) for p in paths))
+        if self._lock.acquire(blocking=False):
+            try:
+                logging.warning("Paths changed: %s",
+                                " ".join(unicode_(p) for p in paths))
+                # TODO : Create Git commit changing these files
+            finally:
+                self._lock.release()
+
+    def _hook_triggered(self, data, conn, addr):
+        if data != b"notsosecret":
+            logging.debug("Got invalid message on hook server from %s",
+                          addr)
+            conn.send(b"hook auth failed\nERROR\n")
+            return
+        logging.info("Hook triggered from %s", addr)
+        if not self._lock.acquire(blocking=False):
+            logging.info("Lock is held, failing...")
+            conn.send(b"update is in progress, try again later\nERROR\n")
+        else:
+            try:
+                conn.send(b"updating directory...\n")
+                # TODO : Write files to directory
+                conn.send(b"synced directory updated!\nOK\n")
+                logging.info("Directory updated")
+            finally:
+                self._lock.release()
 
 
 def synchronize(folder, repository, branchname, timeout):
